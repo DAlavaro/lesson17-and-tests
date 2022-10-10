476559b910cb16eb1ff7b94778ca0be268fd1a77
sdatproizv <d.Alavaro@gmail.com>
2022-10-10 17:02:18 +0300

    10/10/2022W

--- 17_1/02-marshmallow.py
+++ 17_1/02-marshmallow.py
@@ -24,12 +24,42 @@ class UserSchema(Schema):
     age = fields.Int()
 
 
-""" Шаг 2 - делаем дамп в словарь """
-user = User(id=1, name='jane', age=19)
+# """ Шаг 2 - делаем дамп в словарь """
+# user = User(id=1, name='jane', age=19)
+# user_schema = UserSchema()
+# result = user_schema.dump(user)
+# print(type(result))
+# print(result["name"])
+#
+#
+# """ Шаг 3 - делаем дамп в строку """
+# user = User(id=1, name='jane', age=19)
+# user_schema = UserSchema()
+# result = user_schema.dumps(user)
+# print(type(result))
+# print(result)
+
+
+# """ Шаг 4 - делаем дамп списка """
+# user1 = User(id=1, name='jane', age=19)
+# user2 = User(id=2, name='jane2', age=19)
+# user3 = User(id=3, name='jane3', age=19)
+# user4 = User(id=4, name='jane4', age=19)
+# user_schema = UserSchema(many=True)
+# print(user_schema.dump([user1, user2, user3, user4]))
+# print(user_schema.dumps([user1, user2, user3, user4]))
+
+
+""" Шаг 4 - делаем дессереализацию """
+user_json_str = '{"name": "jane", "age": 19}'
 user_schema = UserSchema()
-result = user_schema.dump(user)
-print(type(result))
-print(result["name"])
+
+user_dict = user_schema.loads((user_json_str))
+user = User(**user_dict)
+print(user.age)
+
+
+
 
 
 
