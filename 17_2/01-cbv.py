91a882d7eba54178bcbdb24453335270448f7b81
admin <D.Alavaro@gmail.com>
2022-10-11 23:59:06 +0300

    11.10.2022 H

---
+++ 17_2/01-cbv.py
@@ -0,0 +1,36 @@
+from flask import request, jsonify
+from flask_restx import Resource
+
+books = {
+    1: {
+        "name": "Harry Potter",
+        "year": 2000,
+        "author": "Joan Rouling"
+    },
+    2: {
+        "name": "Le comte de Monte-Cristo",
+        "year": 1844,
+        "author": "Alexandre Dumas"
+    }
+}
+
+
+class BooksView(Resource):
+
+    def get(self):
+        return jsonify(books), 200
+
+    def post(self):
+        req_json = request.json
+        books[len(books) + 1] = req_json
+        return "", 201
+
+
+class BookView(Resource):
+
+    def get(self, bid):
+        return books[bid], 200
+
+    def delete(self, bid):
+        del books[bid]
+        return "", 204
